234e1af7bd92f1373fa89b4cf424809f273a26a2
Bora Özışık <ozisikbora@gmail.com>
2022-03-28 14:59:15 +0300

    Update GlobalFilter.js

--- src/components/GlobalFilter.js
+++ src/components/GlobalFilter.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { TextField,InputAdornment } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+
 
 export const GlobalFilter = ({filter, setFilter}) => {
   return (
         <span>
-            <strong>Search:</strong> {' '}
-            <input className='input' value={filter || ''} onChange = {e => setFilter(e.target.value)} />
+            
+            <TextField label='Search'
+              variant='outlined' 
+              color='secondary' 
+              helperText="Search for a user..." 
+              InputProps={{
+
+                startAdornment: <InputAdornment position='start'><SearchIcon/></InputAdornment>
+              }}
+              fullWidth
+              size='small'
+              value={filter || ''} onChange = {e => setFilter(e.target.value)}/>
+            
             
         </span>
 
