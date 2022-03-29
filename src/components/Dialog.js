9c3238243fe8d9ab63e12a46bcb3f586912b5ad9
Bora Özışık <ozisikbora@gmail.com>
2022-03-30 01:25:38 +0300

    Update Dialog.js

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -5,10 +5,11 @@ import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import { Stack,TextField} from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
-import Typography from '@mui/material/Typography';
+import {useState} from 'react';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -50,6 +51,8 @@ BootstrapDialogTitle.propTypes = {
 
 export default function CustomizedDialogs() {
   const [open, setOpen] = React.useState(false);
+  const[valueLastName,setValueLastName] = useState('')
+  const[valueFirstName,setValueFirstName] = useState('')
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -60,19 +63,46 @@ export default function CustomizedDialogs() {
 
   return (
     <div>
-      <Button variant="contained" onClick={handleClickOpen}>
-        Edit User
-      </Button>
+      
+                                        
+      <EditIcon onClick = {handleClickOpen}/>
+        
+     
       <BootstrapDialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
       >
         <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Modal title
+          Edit User
         </BootstrapDialogTitle>
         <DialogContent dividers>
-            
+        <Stack spacing={2}  >
+
+          <TextField  label='First Name'
+          variant='outlined'
+          color='success'
+          value={valueFirstName}
+          onChange = {e => setValueFirstName(e.target.value)}
+          required 
+          error = {!valueFirstName}
+          helperText={!valueFirstName ? 'Required' : "Please enter a valid first name"}/>                
+          <TextField  
+          label='Last Name'
+          variant='outlined'
+          color='success'
+          value={valueLastName}
+          onChange = {e => setValueLastName(e.target.value)}
+          required 
+          error = {!valueLastName}
+          helperText={!valueLastName ? 'Required' : "Please enter a valid last name"}
+          />
+ 
+          <TextField  label='Gender' variant='outlined' color='success'  helperText="Enter a gender"/>
+          <TextField  label='Date of Birth' variant='outlined' color='success' helperText="Enter date of birth"/>
+          <Button  variant = 'contained' color='info'>Save Changes</Button>
+
+        </Stack>
         </DialogContent>
         
       </BootstrapDialog>
